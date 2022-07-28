4bfa4899ee722f877bd371844de9f6aa6f124575
Saif12121998 <69806597+Saif12121998@users.noreply.github.com>
2022-07-28 11:23:22 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,18 @@
+
+class hello{
+	void m1() {
+		
+	System.out.println("hello world");
+    	}
+	}
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		hello h=new hello();
+		h.m1();
+		h.m1();
+
+	}
+
+}
